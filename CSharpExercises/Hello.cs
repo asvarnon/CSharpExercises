111b72e39784418ff25f272151ab1073ccb9cbf7
asvarnon <austinvarnon@gmail.com>
2021-03-31 15:10:05 -0500

    More experiments fixing main entry point issues

---
+++ CSharpExercises/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class Hello
+	{
+		public void DoHello()
+		{
+			Console.WriteLine("Hello from a class in a namespace!");
+		}
+	}
+}
+
+class Hello
+{
+	public void DoHello()
+	{
+		Console.WriteLine("Hello from a class outside a namespace!");
+	}
+}
--- CSharpExercises/Program.cs
+++ CSharpExercises/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharpExercises
+//removed namespace here to prove it isnt needed
+
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-        }
-    }
+	//ignore static for right now... just easiest way to keep code small
+
+	static Hello hello = new Hello();
+	static ConsoleApp1.Hello hello1 = new ConsoleApp1.Hello();
+
+	//Will not compile
+	//Hello hello2 = new ConsoleApp1.Hello();
+	//ConsoleApp1.Hello hello3 = new Hello();
+	
+	
+	static void Main(string[] args)
+	{
+		hello.DoHello();
+		hello1.DoHello();
+	}
 }
+
