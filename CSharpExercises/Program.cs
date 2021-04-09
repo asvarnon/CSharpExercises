9bfa0cd7b85213b1bf5a74ca79d5b9da7cd23aaf
asvarnon <austinvarnon@gmail.com>
2021-04-08 23:08:40 -0500

    Array Syntax

--- CSharpExercises/Program.cs
+++ CSharpExercises/Program.cs
@@ -48,11 +48,28 @@ public class Program
         //john.lastName = "Johnson";
         //john.introduce();
 
-        Calculator calculator = new Calculator();
-        int result = calculator.add(1, 2);
-        int subtractResult = calculator.subtract(3, 2);
-        Console.WriteLine($"Result is {result}", result);
-        Console.WriteLine($"result of subtraction is {subtractResult}", subtractResult);
+        //Calculator calculator = new Calculator();
+        //int result = calculator.add(1, 2);
+        //int subtractResult = calculator.subtract(3, 2);
+        //Console.WriteLine($"Result is {result}", result);
+        //Console.WriteLine($"result of subtraction is {subtractResult}", subtractResult);
+
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+
+        Console.WriteLine(numbers[0]);
+        Console.WriteLine(numbers[1]);
+        Console.WriteLine(numbers[2]);
+
+        bool[] flags = new bool[3];
+        flags[0] = true;
+
+        Console.WriteLine(flags[0]);
+        Console.WriteLine(flags[1]);
+        Console.WriteLine(flags[2]);
+
+        string[] names = new string[3] {"Jack", "John", "Mary"};
+        Console.WriteLine(names[1]);
     }
 }
 
